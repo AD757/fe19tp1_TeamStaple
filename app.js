34218bec2ee7c5a40a1c0181ae71383e16d66254
Alladin Daher <55769408+AD757@users.noreply.github.com>
2019-11-22 16:22:30 +0100

    app.js
    added the expanded toolbar

--- app.js
+++ app.js
@@ -129,11 +129,28 @@ btnStarred.addEventListener('click', () => {
 })
 
 // ********* QUILL ********* //
+var toolbarOptions = [
+   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+   ['blockquote', 'code-block'],
+   [{ 'header': [1, 2, 3, 4, 5, 6, false] }],               // custom button values
+   [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+   [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+   [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+   [{ 'direction': 'rtl' }],                         // text direction
+   [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+   [ 'link', 'image', 'video', 'formula'],
+   [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+   [{ 'font': [] }],
+   [{ 'align': [] }],
+   ['clean']                                         // remove formatting button
+ ];
 const quill = new Quill('#editor', {
-    theme: 'snow',
-    placeholder: 'Type your note here...',
-});
-
+   modules: {
+     toolbar: toolbarOptions
+   },
+   theme: 'snow',
+   placeholder: 'Type your note here...',
+ });
 // ********* LOCAL STORAGE ********* //
 
 //An empty Array to save New Notes Objects into
